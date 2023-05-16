ad4f88250b686faa066c2f28cf8ec79b54912b78
Saurabh Gaur <saurabhgaur@gmail.com>
2023-05-16 22:54:18 +0530

    formatting changes to main.py

--- main.py
+++ main.py
@@ -8,19 +8,26 @@ from langchain.llms import OpenAI
 from langchain.agents import create_csv_agent
 from langchain.llms import OpenAI
 
+from langchain.agents import load_tools
+
+
 
 def load_agent():
     """Logic for loading the chain you want to use should go here."""
-    agent = create_csv_agent(OpenAI(temperature=0), "./Orphanet-en_product4.csv", verbose=True)
+    agent = create_csv_agent(
+        OpenAI(temperature=0), "./Orphanet-en_product4.csv", verbose=True
+    )
 
     llm = OpenAI(temperature=0)
     return agent
+
 
 agent = load_agent()
 
 # From here down is all the StreamLit UI.
 st.set_page_config(page_title="RGDs Search", page_icon=":robot:")
 st.header("RGDs Search")
+st.text("A chatbot to help you search rare genetic diseases based on symptoms")
 
 if "generated" not in st.session_state:
     st.session_state["generated"] = []
@@ -30,7 +37,9 @@ if "past" not in st.session_state:
 
 
 def get_text():
-    input_text = st.text_input("You: ", "Hello, how are you?", key="input")
+    input_text = st.text_input(
+        "You: ", "How many disesases do you have in your database?", key="input"
+    )
     return input_text
 
 
@@ -43,7 +52,6 @@ if user_input:
     st.session_state.generated.append(output)
 
 if st.session_state["generated"]:
-
     for i in range(len(st.session_state["generated"]) - 1, -1, -1):
         message(st.session_state["generated"][i], key=str(i))
         message(st.session_state["past"][i], is_user=True, key=str(i) + "_user")
